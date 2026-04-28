0f65d79ab92a2337f7d2611cfe40157624eee18a
romerafj <96294315+romerafj@users.noreply.github.com>
2026-04-28 09:10:11 +0200

    Update wsgi.py

--- cn/wsgi.py
+++ cn/wsgi.py
@@ -6,9 +6,13 @@ import django
 django.setup()
 
 from django.core.management import call_command
-
-# Ejecutar migraciones automáticamente
 call_command('migrate', interactive=False)
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
 
 from django.core.wsgi import get_wsgi_application
 
