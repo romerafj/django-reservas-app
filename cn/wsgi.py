5c1b73c62b811112de0edbfeaa845946c7971866
romerafj <96294315+romerafj@users.noreply.github.com>
2026-04-28 09:27:01 +0200

    Update wsgi.py

--- cn/wsgi.py
+++ cn/wsgi.py
@@ -5,15 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cn.settings')
 import django
 django.setup()
 
-from django.core.management import call_command
-call_command('migrate', interactive=False)
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
